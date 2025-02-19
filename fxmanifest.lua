1eba9e3801a51418bac044d0d3ba28e5a4570b45
addzodus <addzodus@gmail.com>
2025-02-20 02:50:03 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,18 +4,4 @@ game 'gta5'
 
 description 'dev by addzodus - no melee finisher move'
 lua54 'yes'
-version '2.3'
-
-shared_script {'config.lua','@ox_lib/init.lua', }
-
-client_scripts {
-	'client/*.lua',
-}
-
-server_scripts {
-	'server/*.lua',
-}
-
-escrow_ignore {
-	'config.lua'
-}
+version '2.3.0'
